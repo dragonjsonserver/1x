35bfc7b92b363d465b5ea7a687bddcc6036579f5
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-07-22 20:17:13 +0000

    Neuigkeit zur Veröffentlichung der Version 1.1.0

--- config/dragonx/homepage/news.php
+++ config/dragonx/homepage/news.php
@@ -22,13 +22,25 @@ return array(
     'perrow' => 2,
     'news' => array(
         array(
+            'title' => 'Version 1.1.0 zum Download verfügbar',
+            'content' =>
+                  'Die Version 1.1.0 ist abgeschlossen und als Download '
+                . 'verfügbar. Die größten Neuerungen sind die neuen Pakete '
+                . 'DragonX_Log und DragonX_Cronjob, die Unterstützung von '
+                . 'Multirequests (Bündelung mehrerer Serviceanfragen in einem '
+                . 'HTTP Request) und die Verwaltung der Datenbankstruktur '
+                . 'durch Installationsplugins.',
+            'timestamp' => '22.07.2012 22:16 Uhr',
+        ),
+        array(
             'title' => 'Version 1.0.0 zum Download verfügbar',
             'content' =>
-            'Die Version 1.0.0 ist abgeschlossen und ist als Download '
-                . 'verfügbar. Als Downloadpaket gibt es nur die '
-                . 'Projektvorlage mit allen Projektdateien, erst für spätere '
-                . 'Versionen wird es auch Upgradepakete geben. Ebenfalls '
-                . 'abgeschlossen ist der SVN Commit bei Google Code.',
+                  'Die Version 1.0.0 ist abgeschlossen und als Download '
+                . 'verfügbar. Mit im Downloadpaket der Projektvorlage '
+                . 'enthalten sind alle Grundfunktionalitäten zur Erstellung '
+                . 'eines Json Servers, der generische Json Client und '
+                . 'optionale Pakete für die Homepage, die Datenbank und einer '
+                . 'einfachen Accountverwaltung.',
             'timestamp' => '07.07.2012 14:22 Uhr',
         ),
         array(
@@ -37,10 +49,10 @@ return array(
                   'Die Grundstruktur des Projektes steht, die Demo ist '
                 . 'funktionsfähig, die Anmeldung bei Google Code ist '
                 . 'abgeschlossen, die Domain ist in Arbeit, die Open Source '
-                . 'Lizenz ist ausgewählt. Kurz: Das Projekt ist also '
-                . 'gestartet. Die nächsten Tage noch die letzten Arbeiten an '
-                . 'einer ersten Vorabversion durchführen und der erste '
-                . 'Download kann Online gestellt werden.',
+                . 'Lizenz ist ausgewählt. Kurz: Das Projekt ist gestartet. '
+                . 'Die nächsten Tage noch die letzten Arbeiten an '
+                . 'einer ersten Version durchführen und der erste Download '
+                . 'kann Online gestellt werden.',
             'timestamp' => '08.06.2012 22:19 Uhr',
         ),
     )
