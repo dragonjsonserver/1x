44d38a33159daeb4885457a87201428679841d46
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-08-08 18:49:12 +0000

    Releaseangaben für die Version 1.1.1

--- config/dragonx/homepage/changelog.php
+++ config/dragonx/homepage/changelog.php
@@ -19,9 +19,9 @@
  */
 return array(
     'Changelog',
+    '1.1.1' => 'v1.1.1',
     '1.1.0' => 'v1.1.0',
     '1.0.0' => 'v1.0.0',
     'Roadmap',
-    '1.1.1' => 'v1.1.1',
     'roadmap' => 'Ideensammlung',
 );
