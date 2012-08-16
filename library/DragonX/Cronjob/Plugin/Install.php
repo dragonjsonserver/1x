43f530c06d28fb3f2bcf5c1ae6b0195d4543fc85
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-08-16 15:18:59 +0000

    Installationsplugins für die Migration von Version 1.1.x auf 1.2.0

--- library/DragonX/Cronjob/Plugin/Install.php
+++ library/DragonX/Cronjob/Plugin/Install.php
@@ -28,16 +28,33 @@ class DragonX_Cronjob_Plugin_Install
     public function getInstall($oldversion = '0.0.0')
     {
         $sqlstatements = array();
-        if (version_compare($oldversion, '1.0.0', '<')) {
-            $sqlstatements[] =
-                "CREATE TABLE `dragonx_cronjob_record_cronjob` ("
-                  . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
-                  . "`pluginname` VARCHAR(255) NOT NULL, "
-                  . "`count` INT(10) UNSIGNED NOT NULL, "
-                  . "`timestamp` INT(10) UNSIGNED NOT NULL, "
-                  . "PRIMARY KEY (`id`), "
-                  . "UNIQUE KEY (`pluginname`) "
+        if (version_compare($oldversion, '1.1.0', '<')) {
+            $sqls[] =
+                  "CREATE TABLE `dragonx_cronjob_cronjobs` ("
+                    . "`cronjobid` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`pluginname` VARCHAR(255) NOT NULL, "
+                    . "`count` VARCHAR(255) NOT NULL, "
+                    . "`firsttimestamp` TIMESTAMP NOT NULL, "
+                    . "`lasttimestamp` TIMESTAMP NOT NULL, "
+                    . "PRIMARY KEY (`cronjobid`), "
+                    . "UNIQUE KEY (`pluginname`) "
                 . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        }
+        if (version_compare($oldversion, '1.2.0', '<')) {
+            $sqlstatements[] =
+                  "CREATE TABLE `dragonx_cronjob_record_cronjob` ("
+                    . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`pluginname` VARCHAR(255) NOT NULL, "
+                    . "`count` INT(10) UNSIGNED NOT NULL, "
+                    . "`timestamp` INT(10) UNSIGNED NOT NULL, "
+                    . "PRIMARY KEY (`id`), "
+                    . "UNIQUE KEY `pluginname` (`pluginname`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+
+            $sqlstatements[] =
+                  "INSERT INTO `dragonx_cronjob_record_cronjob` (`id`, `pluginname`, `count`, `timestamp`) "
+                . "SELECT `cronjobid`, `pluginname`, `count`, UNIX_TIMESTAMP(`lasttimestamp`) FROM `dragonx_cronjob_cronjobs`";
+            $sqlstatements[] = "DROP TABLE `dragonx_cronjob_cronjobs`";
         }
         return $sqlstatements;
     }
