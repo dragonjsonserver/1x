408b2c1ef4ad248f0ecdc0c29bd27d2cf7781824
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-09-01 15:00:12 +0000

    Logikklasse und abstrakter Record für Nested Set Strukturen ACL Implementierung mit Ressourcen und Rollen als Nested Sets und Plugin das Annotations von Services prüft

---
+++ library/DragonX/Acl/Plugin/Install.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Plugin zur Installation des Paketes
+ */
+class DragonX_Acl_Plugin_Install implements DragonX_Storage_Plugin_Install_Interface
+{
+    /**
+     * Gibt die SQL Statements zurück um das Paket zu updaten
+     * @param string $oldversion
+     * @return array
+     */
+    public function getInstall($oldversion = '0.0.0')
+    {
+        $sqlstatements = array();
+        if (version_compare($oldversion, '1.3.0', '<')) {
+            $sqlstatements[] =
+                  "CREATE TABLE `dragonx_acl_record_resource` ("
+			        . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+			        . "`name` VARCHAR(255) NOT NULL, "
+			        . "`lft` INT(10) UNSIGNED NOT NULL, "
+			        . "`rgt` INT(10) UNSIGNED NOT NULL, "
+			        . "PRIMARY KEY (`id`), "
+			        . "KEY (`lft`), "
+			        . "KEY (`rgt`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+            $sqlstatements[] =
+                  "INSERT INTO `dragonx_acl_record_resource` (`name`, `lft`, `rgt`) "
+                . "VALUES ('All', 1, 2)";
+
+            $sqlstatements[] =
+                  "CREATE TABLE `dragonx_acl_record_role` ("
+                    . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`name` VARCHAR(255) NOT NULL, "
+                    . "`lft` INT(10) UNSIGNED NOT NULL, "
+                    . "`rgt` INT(10) UNSIGNED NOT NULL, "
+                    . "PRIMARY KEY (`id`), "
+                    . "KEY (`lft`), "
+                    . "KEY (`rgt`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+            $sqlstatements[] =
+                  "INSERT INTO `dragonx_acl_record_role` (`name`, `lft`, `rgt`) "
+                . "VALUES ('All', 1, 2)";
+
+            $sqlstatements[] =
+                  "CREATE TABLE `dragonx_acl_record_roleresource` ("
+                    . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`roleid` INT(10) UNSIGNED NOT NULL, "
+                    . "`resourceid` INT(10) UNSIGNED NOT NULL, "
+                    . "PRIMARY KEY (`id`), "
+                    . "UNIQUE KEY (`roleid`, `resourceid`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+
+            $sqlstatements[] =
+                  "CREATE TABLE `dragonx_acl_record_accountrole` ("
+                    . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`accountid` INT(10) UNSIGNED NOT NULL, "
+                    . "`roleid` INT(10) UNSIGNED NOT NULL, "
+                    . "PRIMARY KEY (`id`), "
+                    . "UNIQUE KEY (`accountid`, `roleid`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        }
+        return $sqlstatements;
+    }
+}
