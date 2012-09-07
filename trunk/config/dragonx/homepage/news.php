50dd19421a4288c7c6d0c41df024381a517f02ee
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-09-07 17:34:52 +0000

    Umstellung des Trunks auf den nächsten Branch 1.6

--- trunk/config/dragonx/homepage/news.php
+++ trunk/config/dragonx/homepage/news.php
@@ -22,6 +22,13 @@ return array(
     'perrow' => 2,
     'news' => array(
         array(
+            'title' => 'Version 1.6.0 zum Download verfügbar',
+            'content' =>
+                  'Die Version 1.6.0 ist abgeschlossen und als Download '
+                . 'verfügbar.',
+            'timestamp' => 0,
+        ),
+        array(
             'title' => 'Version 1.5.0 zum Download verfügbar',
             'content' =>
                   'Die Version 1.5.0 ist abgeschlossen und als Download '
