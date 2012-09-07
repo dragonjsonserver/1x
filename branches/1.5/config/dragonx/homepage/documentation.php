3401c00242c2c7345f6f9d7126cd2d308a842d56
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-09-07 17:25:52 +0000

    Branch 1.5

---
+++ branches/1.5/config/dragonx/homepage/documentation.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * @return array
+ */
+return array(
+    'Grundstruktur',
+    'framework_architecture' => 'Architektur der Anwendung',
+    'framework_firststeps' => 'Erste Schritte',
+    'framework_dragonjsonclient' => 'DragonJsonClient',
+    'framework_ownpackages' => 'Eigene Pakete',
+    'framework_jsonclient' => 'Nutzung des Json Client',
+    'Namensraum Dragon',
+    'dragon_application' => 'Paket Application',
+    'dragon_json' => 'Paket Json',
+    'dragon_package' => 'Paket Package',
+    'dragon_plugin' => 'Paket Plugin',
+    'Namensraum DragonX',
+    'dragonx_account' => 'Paket Account',
+    'dragonx_acl' => 'Paket Acl',
+    'dragonx_clientmessage' => 'Paket Clientmessage',
+    'dragonx_cronjob' => 'Paket Cronjob',
+    'dragonx_homepage' => 'Paket Homepage',
+    'dragonx_log' => 'Paket Log',
+    'dragonx_nestedset' => 'Paket Nested Set',
+    'dragonx_storage' => 'Paket Storage',
+);
