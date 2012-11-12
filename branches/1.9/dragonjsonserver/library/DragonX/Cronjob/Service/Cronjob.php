e986dd7f415d6891a85d14c5affea7ffa3c90dae
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-11-12 21:08:38 +0000

    Branch 1.9

---
+++ branches/1.9/dragonjsonserver/library/DragonX/Cronjob/Service/Cronjob.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Serviceklasse zur Verwaltung und Ausführung von Cronjobs
+ */
+class DragonX_Cronjob_Service_Cronjob
+{
+    /**
+     * Führt einen Cronjob aus auch wenn sein Intervall noch nicht erreicht ist
+     * @param string $securitytoken
+     * @param string $pluginname
+     * @throws InvalidArgumentException
+     */
+    public function executeCronjob($securitytoken, $pluginname)
+    {
+        $configCronjob = new Dragon_Application_Config('dragonx/cronjob/cronjob');
+        if ($configCronjob->securitytoken != $securitytoken) {
+            throw new Dragon_Application_Exception('incorrect securitytoken');
+        }
+
+        $logicCronjob = new DragonX_Cronjob_Logic_Cronjob();
+        $logicCronjob->executeCronjob($pluginname);
+    }
+
+    /**
+     * Führt alle Cronjobs deren Intervall erreicht wurde aus
+     * @param string $securitytoken
+     * @throws InvalidArgumentException
+     */
+    public function executeCronjobs($securitytoken)
+    {
+        $configCronjob = new Dragon_Application_Config('dragonx/cronjob/cronjob');
+        if ($configCronjob->securitytoken != $securitytoken) {
+            throw new Dragon_Application_Exception('incorrect securitytoken');
+        }
+
+        $logicCronjob = new DragonX_Cronjob_Logic_Cronjob();
+        $logicCronjob->executeCronjobs();
+    }
+}
