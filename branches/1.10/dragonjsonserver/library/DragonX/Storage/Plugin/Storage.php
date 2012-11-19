465495e985a5e6489101c56bd4c36d4b1c9a89c1
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-11-19 21:03:33 +0000

    Branch 1.10

---
+++ branches/1.10/dragonjsonserver/library/DragonX/Storage/Plugin/Storage.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Plugin zur Initialisierung der Storage Engine bei jedem Request
+ */
+class DragonX_Storage_Plugin_Storage implements Dragon_Application_Plugin_Bootstrap_Interface
+{
+    /**
+     * Initialisiert bei jedem Request die Storage Engine
+     */
+    public function bootstrap()
+    {
+        $configEngine = new Dragon_Application_Config('dragonx/storage/engine');
+        if (isset($configEngine->engine)) {
+        	Zend_Registry::set('DragonX_Storage_Engine', $configEngine->engine);
+        } else {
+            $configEngines = $configEngine;
+            foreach ($configEngines as $storagekey => $configEngine) {
+                Zend_Registry::set($storagekey, $configEngine->engine);
+            }
+        }
+    }
+}
