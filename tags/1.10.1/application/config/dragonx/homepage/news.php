7b682b8e61d2ee191bc61bfa48084bc7e0358352
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2012-11-21 18:20:40 +0000

    Tag 1.10.1

---
+++ tags/1.10.1/application/config/dragonx/homepage/news.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * @return array
+ */
+return array(
+    'perpage' => 4,
+    'perrow' => 2,
+    'news' => array(
+        array(
+            'title' => 'Version 1.7.0 zum Download verfügbar',
+            'content' =>
+                  'Die Version 1.7.0 der Projektvorlage ist abgeschlossen und '
+                . 'als Download verfügbar. In der Version wurde die '
+                . 'Einstellungsdatei \'/config/packagenamespaces.php\' '
+                . 'erweitert um standardmäßig alle Plugins und Services der '
+                . 'Namensräume Dragon und DragonX einzubinden.',
+            'timestamp' => 0,
+        ),
+        array(
+            'title' => 'Version 1.6.0 zum Download verfügbar',
+            'content' =>
+                  'Die Version 1.6.0 der Projektvorlage ist abgeschlossen und '
+                . 'als Download verfügbar. In der Version wurde die '
+                . 'Projektvorlage als einfacher Einstieg für eigene Projekte '
+                . 'auf Basis des DragonJsonServers erstellt.',
+            'timestamp' => 1347580549,
+        ),
+    ),
+);
---
+++ tags/1.10.1/dragonjsonserver/library/Dragon/Repository/Registry.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Klasse zur Verwaltung der aller Repositories
+ */
+class Dragon_Repository_Registry
+{
+    /**
+     * @var array
+     */
+    private $_repositories = array();
+
+    /**
+     * Fügt mehrere Repositories hinzu zum Abfrufen
+     * @param Dragon_Application_Application $application
+     * @param array $repositories
+     */
+    public function __construct(Dragon_Application_Application $application, array $repositories)
+    {
+    	foreach ($repositories as $directorypath) {
+    		$application->addLibrarypath($directorypath . '/library');
+    	}
+    	$this->_repositories = $repositories;
+    }
+
+    /**
+     * Gibt den Pfad zum übergebenen Repository zurück
+     * @param string $repositoryname
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    public function getRepository($repositoryname)
+    {
+    	if (!isset($this->_repositories[$repositoryname])) {
+    		throw new Dragon_Application_Exception_System('incorrect repositoryname', array('repositoryname' => $repositoryname));
+    	}
+        return $this->_repositories[$repositoryname];
+    }
+
+    /**
+     * Gibt alle registrierten Repositories zurück
+     * @return array
+     */
+    public function getRepositories()
+    {
+        return $this->_repositories;
+    }
+}
