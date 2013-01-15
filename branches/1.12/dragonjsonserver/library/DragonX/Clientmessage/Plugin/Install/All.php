1273046ea5aa700218dc0e63be7e8cbd037eeb5c
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2013-01-15 22:35:58 +0000

    Branch 1.12

---
+++ branches/1.12/dragonjsonserver/library/DragonX/Clientmessage/Plugin/Install/All.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Plugin zur Installation des Paketes
+ */
+class DragonX_Clientmessage_Plugin_Install_All implements DragonX_Storage_Plugin_Install_Interface
+{
+    /**
+     * Installiert das Plugin in der übergebenen Datenbank
+     * @param DragonX_Storage_Engine_ZendDbAdataper $storage
+     * @param string $version
+     */
+    public function install(DragonX_Storage_Engine_ZendDbAdataper $storage, $version = '0.0.0')
+    {
+        if (version_compare($version, '1.8.0', '<')) {
+            $storage->executeSqlStatement(
+                  "CREATE TABLE `dragonx_clientmessage_record_all` ("
+                    . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
+                    . "`created` INT(10) UNSIGNED NOT NULL, "
+                    . "`key` VARCHAR(255) NOT NULL, "
+                    . "`result` TEXT NOT NULL, "
+                    . "PRIMARY KEY (`id`), "
+                    . "KEY (`created`)"
+                . ") ENGINE=InnoDB DEFAULT CHARSET=utf8"
+            );
+        }
+    }
+}
---
+++ branches/1.12/dragonjsonserver/library/DragonX/Log/Logic/Log.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled with this
+ * package in the file LICENSE.txt. It is also available through the
+ * world-wide-web at this URL: http://dragonjsonserver.de/license. If you did
+ * not receive a copy of the license and are unable to obtain it through the
+ * world-wide-web, please send an email to license@dragonjsonserver.de. So we
+ * can send you a copy immediately.
+ *
+ * @copyright Copyright (c) 2012 DragonProjects (http://dragonprojects.de)
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ * @author Christoph Herrmann <developer@dragonjsonserver.de>
+ */
+
+/**
+ * Logik zur Speicherung von Logeinträgen
+ */
+class DragonX_Log_Logic_Log
+{
+	/**
+	 * Loggt eine Ausnahme mit automatischer Bestimmung des Schweregrades
+	 * @param Exception $exception
+	 */
+	public function automatic(Exception $exception)
+	{
+        $configException = new Dragon_Application_Config('dragonx/log/exception');
+        foreach ($configException as $classname => $method) {
+        	if ($exception instanceof $classname) {
+        		if (isset($method)) {
+        		    $this->__call($method, array($exception));
+        		}
+        		return;
+        	}
+        }
+	}
+
+    /**
+     * Speichert einen neuen Logeintrag
+     * @param string $method
+     * @param array $params
+     * @throws Zend_Log_Exception
+     */
+    public function __call($method, $params)
+    {
+    	if (!Zend_Registry::isRegistered('Zend_Log')) {
+    		return;
+    	}
+	    $extras = null;
+        switch (count($params)) {
+            case 0:
+                throw new Dragon_Application_Exception_System('missing message');
+            case 1:
+            	$param = array_shift($params);
+            	if ($param instanceof Exception) {
+            		$message = $param->getMessage();
+            		$extras = array(
+            			'params' => array(
+	            			'code' => $param->getCode(),
+	            			'file' => $param->getFile(),
+	            			'line' => $param->getLine(),
+	            			'trace' => $param->getTrace(),
+            			)
+            		);
+            		if ($param instanceof Dragon_Application_Exception_Abstract) {
+            			$extras['params'] += array(
+            				'data' => $param->getData(),
+            			);
+            		}
+            	} else {
+	                $message = $param;
+            	}
+                break;
+            default:
+                $message = array_shift($params);
+                $extras = array('params' => array_shift($params));
+                break;
+        }
+        if (isset($extras) && is_array($extras['params'])) {
+        	$extras['params'] = Zend_Json::encode($extras['params']);
+        }
+        $logger = Zend_Registry::get('Zend_Log');
+        $logger->__call($method, array($message, $extras));
+    }
+}
