7e1532570a18d660cfb41b3385d6a1bb6558d6ef
dragonprojects@gmail.com <dragonprojects@gmail.com@2272378f-fb8a-1b90-f2bf-038ccc9fc729>
2013-02-25 14:33:04 +0000

    Beim Installieren der Packages kann nun angegeben werden welche Storage Engines berücksichtigt werden sollen

--- dragonjsonserver/library/DragonX/Storage/Logic/Database.php
+++ dragonjsonserver/library/DragonX/Storage/Logic/Database.php
@@ -19,18 +19,18 @@
  */
 class DragonX_Storage_Logic_Database
 {
-	/**
-	 * @var array
-	 */
-	private $_listPlugins = array();
+    /**
+     * @var array
+     */
+    private $_listPlugins = array();
 
     /**
      * Initialisiert die Struktur der Paketliste in der Datenbank
      * @param string $storagekey
      */
-	private function _install($storagekey)
-	{
-	    $storage = Zend_Registry::get($storagekey);
+    private function _install($storagekey)
+    {
+        $storage = Zend_Registry::get($storagekey);
         $storage->executeSqlStatement(
               "CREATE TABLE IF NOT EXISTS `dragonx_storage_record_plugin` ("
                 . "`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, "
@@ -42,28 +42,29 @@ class DragonX_Storage_Logic_Database
                 . "UNIQUE KEY (`pluginname`)"
             . ") ENGINE=InnoDB DEFAULT CHARSET=utf8"
         );
-	}
+    }
 
-	/**
-	 * Gibt die Pluginliste der jeweiligen Datenbank zurück
-	 * @param string $storagekey
-	 * @return array
-	 */
-	private function _getListPlugins($storagekey)
-	{
+    /**
+     * Gibt die Pluginliste der jeweiligen Datenbank zurück
+     * @param string $storagekey
+     * @return array
+     */
+    private function _getListPlugins($storagekey)
+    {
         if (!isset($this->_listPlugins[$storagekey])) {
             $this->_install($storagekey);
-	        $this->_listPlugins[$storagekey] = Zend_Registry::get($storagekey)
-	            ->loadByConditions(new DragonX_Storage_Record_Plugin())
-	            ->indexBy('pluginname', true);
+            $this->_listPlugins[$storagekey] = Zend_Registry::get($storagekey)
+                ->loadByConditions(new DragonX_Storage_Record_Plugin())
+                ->indexBy('pluginname', true);
         }
         return $this->_listPlugins[$storagekey];
-	}
+    }
 
     /**
      * Installiert mit den Plugins die Packages und trägt die Versionen ein
+     * @param array $storagekeys
      */
-    public function installPackages()
+    public function installPackages($storagekeys = null)
     {
         $plugins = Zend_Registry::get('Dragon_Plugin_Registry')->getPlugins('DragonX_Storage_Plugin_Install_Interface');
         foreach ($plugins as $plugin) {
@@ -71,16 +72,19 @@ class DragonX_Storage_Logic_Database
             if ($plugin instanceof DragonX_Storage_Plugin_GetStoragekey_Interface) {
                 $storagekey = $plugin->getStoragekey();
             }
+            if (isset($storagekeys) && !in_array($storagekey, $storagekeys)) {
+                continue;
+            }
             $listPlugins = $this->_getListPlugins($storagekey);
             $pluginname = get_class($plugin);
             $version = '0.0.0';
             if (isset($listPlugins[$pluginname])) {
-            	$version = $listPlugins[$pluginname]->version;
+                $version = $listPlugins[$pluginname]->version;
             }
             $storage = Zend_Registry::get($storagekey);
             $storage->beginTransaction();
             try {
-            	$plugin->install($storage, $version);
+                $plugin->install($storage, $version);
                 list ($packagenamespace, $packagename) = explode('_', $pluginname, 3);
                 $classname = $packagenamespace . '_' . $packagename . '_Version';
                 $version = new $classname();
@@ -96,7 +100,7 @@ class DragonX_Storage_Logic_Database
                         'version' => $version->getVersion(),
                     )));
                 }
-            	$storage->commit();
+                $storage->commit();
             } catch (Exception $exception) {
                 $storage->rollback();
                 throw $exception;
